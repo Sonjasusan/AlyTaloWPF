2409d8a9c19e8242116159295fa2ecbb9d89a4f7
Sonjasusan <sonja.nyman92@gmail.com>
2022-09-22 14:04:39 +0300

    Älytalon testitapaukset (lämpö ja valot)

---
+++ AlyTaloWPF.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlyTaloWPF.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlyTaloWPF.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c01b7d96-ff67-4226-bef9-9b69a692b84d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AlyTaloWPF.Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlyTaloWPF.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TalonLampoTesti()// Talon lämpötila voidaan asettaa vähintään 19 ja max 32
+        {
+            Thermostat t = new Thermostat();
+
+            t.Temperature = 19;
+
+            try
+            {
+                t.Temperature = 8;  //Yritetään asettaa lämmöksi 8
+
+                Assert.Fail("Poikkeuksen nosto epäonnistui");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //Poikkeuksen nosto onnistui - Ohjelmassa pyytää antamaan uuden arvon josse on alle 19 tai yli 32
+            }
+
+        }
+
+        [TestMethod]//Saunan lämmitys katkeaa kun se saavuttaa 100 astetta
+        public void SaunaTesti()
+        {
+            Sauna saunanLampo = new Sauna();
+            saunanLampo.NykyinenLampo = 100;
+            try
+            {
+                saunanLampo.NykyinenLampo = 104;
+
+                Assert.Fail("Poikkeusta ei ole");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //Onnistui - poikkeusta ei ole
+
+            }
+        }
+
+        [TestMethod]
+       public void ValoSliderTesti() //Valonslideri maximi on 100
+        {
+            Lights sliderivalue = new Lights();
+            sliderivalue.SwitchON(90);
+            try
+            {
+                sliderivalue.SwitchON(110);
+                Assert.Fail("Epäonnistui");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //Onnistui, otettiin exception kiinni
+            }
+        }
+    }
+}
